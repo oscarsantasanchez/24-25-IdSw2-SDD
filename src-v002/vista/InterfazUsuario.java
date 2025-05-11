dc85271046c2a3d736b20f9d3fd6a4b88afb5831
Dievex <diego30sscc@gmail.com>
2025-05-11 11:33:39 +0200

    refactor: move iniciar method to InterfazUsuario for clarity

--- src-v002/vista/InterfazUsuario.java
+++ src-v002/vista/InterfazUsuario.java
@@ -1,27 +1,42 @@
 package vista;
 
-import controlador.Teclado;
 import modelo.Matriz;
 import modelo.Posicion;
+import controlador.Teclado;
+import controlador.ControladorHoja;
+
 public class InterfazUsuario {
     private Teclado teclado;
     private VisualizadorHoja visualizador;
     
-    public InterfazUsuario(Matriz matriz, Posicion posicion) {
-        this.matriz = matriz;
-        this.posicion = posicion;
+    public InterfazUsuario() {
         this.teclado = new Teclado();
         this.visualizador = new VisualizadorHoja();
     }
-
+    
     public Teclado getTeclado() {
         return teclado;
     }
-
-
-    public void actualizarVista() {
-        visualizador.mostrarHoja();
-        visualizador.mostrarInfoCelda();
+    
+    public void actualizarVista(Matriz matriz, Posicion posicion) {
+        visualizador.mostrarHoja(matriz, posicion);
+        visualizador.mostrarInfoCelda(matriz, posicion);
         visualizador.mostrarInstrucciones();
+    }
+    
+    public void iniciar(Matriz matriz, Posicion posicion, ControladorHoja controlador) {
+        Teclado teclado = this.getTeclado();
+        boolean ejecutando = true;
+        
+        while (ejecutando) {
+            this.actualizarVista(matriz, posicion);
+            
+            char comando = teclado.leerComando();
+            
+            ejecutando = controlador.procesarComando(comando, teclado);
+        }
+        
+        teclado.cerrar();
+        System.out.println("Programa finalizado.");
     }
 }
